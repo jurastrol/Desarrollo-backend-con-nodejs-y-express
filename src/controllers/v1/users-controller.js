1172f42fc6bc48d14a84d4cd4617b23d807d2745
Juan Luis Rastrollo Guerrero <juanlu@MacBook-Pro-de-Juan.local>
2020-02-29 12:50:50 +0100

    Versión 1.0

--- src/controllers/v1/users-controller.js
+++ src/controllers/v1/users-controller.js
@@ -1,6 +1,25 @@
 const bcrypt = require('bcrypt');
 const Users = require('../../mongo/models/users');
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    if (user) {
+      const isOK = await bcrypt.compare(password, user.password);
+      if (isOK) {
+        res.send({ status: 'OK', data: {} });
+      } else {
+        res.status(403).send({ status: 'INVALID PASSWORD', message: '' });
+      }
+    } else {
+      res.status(401).send({ status: 'USER NOT FOUND', message: '' });
+    }
+  } catch (e) {
+    res.status(500).send({ status: 'ERROR', message: e.message });
+  }
+};
+
 const createUser = async (req, res) => {
   try {
     const { username, email, password, data } = req.body;
@@ -46,7 +65,7 @@ const getUsers = (req, res) => {
 const updateUser = async (req, res) => {
   try {
     const { username, email, data, userId } = req.body;
-    await Users.findByIdAndUpdate(userId, {username, email, data});
+    await Users.findByIdAndUpdate(userId, { username, email, data });
     res.send({ status: 'OK', message: 'user updated' });
   } catch (error) {
     if (error.code && error.code === 11000) {
@@ -64,5 +83,6 @@ module.exports = {
   createUser,
   deleteUser,
   getUsers,
-  updateUser
+  updateUser,
+  login
 };
